77ecc156554d6b8a34f6b1f5e8ea9cbef80fbe38
Sainitin D <s.daverpally@gmail.com>
2024-08-01 21:05:25 -0400

    add initial vue project and add navbar

---
+++ personal-portfolio/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <NavBar />
+</template>
+
+<script setup lang="ts">
+import NavBar from './components/NavBar.vue';
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  /* margin: 0px;
+  padding: 0rem;
+  box-sizing: border-box */
+}
+</style>
---
+++ personal-portfolio/src/components/NavBar.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="desktop-nav">
+        <div id="logo">Sainitin Daverpally</div>
+        <div class="nav-links">
+            <button><a href="#about">About</a></button>
+            <button><a href="#projects">Projects</a></button>
+            <button><a href="#work">Work Experience</a></button>
+            <button><a href="#recommendations">Skills</a></button>
+            <button><a href="#contact">Contact</a></button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+#desktop-nav {
+    display: flex;
+    justify-content: space-between;
+    margin: 0 2rem;
+    margin-top: 1rem;
+}
+
+.nav-links {
+    display: flex;
+    gap: 1rem;
+    margin: 0;
+    padding: 0;
+}
+
+#logo {
+  font-size: 2rem;
+}
+
+#logo:hover {
+  cursor: default;
+}
+</style>
