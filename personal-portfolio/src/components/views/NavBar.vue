6a12332c9cfb03aad8b02d12e74c8182c62e3453
Sainitin D <s.daverpally@gmail.com>
2024-08-06 18:30:21 -0400

    add navbar

---
+++ personal-portfolio/src/components/views/NavBar.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="desktop-nav">
+        <a href="#home">Home</a>
+        <p>*</p>
+        <a href="#projects">Projects</a>
+        <p>*</p>
+        <a href="#work">Work</a>
+        <p>*</p>
+        <a href="#skills">Skills</a>
+        <p>*</p>
+        <a href="/resume">Resume</a>
+        <p>*</p>
+        <a href="#contact">Contact</a>
+    </div>
+</template>
+
+<style scoped>
+#desktop-nav {
+    margin: 0 2rem;
+    margin-top: 1rem;
+    display: flex;
+    gap: 1rem;
+    position: fixed;
+    right: 0;
+    margin-right: 24px;
+}
+/* 
+#logo {
+  font-size: 2rem;
+}
+
+#logo:hover {
+  cursor: default;
+} */
+</style>
