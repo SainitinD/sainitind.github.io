ab3bdd0e3c2d14997a3a1751c52ac9a62f73eda9
Sainitin D <s.daverpally@gmail.com>
2024-08-12 21:06:20 -0400

    modify some styling to project cards

--- personal-portfolio/src/components/projects/ThumbnailCard.vue
+++ personal-portfolio/src/components/projects/ThumbnailCard.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="margin-overlay">
-        <div class="card">
-            <img class="thumbnail" :src="imgSrc" />
-            <div class="info">
-                <p class="title">{{ title }}</p>
-                <p class="category">{{ category }}</p>
-            </div>
-        </div>
+  <div class="margin-overlay">
+    <div class="card">
+      <img class="thumbnail" :src="imgSrc" />
+      <div class="info">
+        <p class="title">{{ title }}</p>
+        <p class="category">{{ category }}</p>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -20,46 +20,48 @@ defineProps({
 
 <style scoped>
 .card {
-    background-color: #8E6FFB;
-    border-radius: 5%;
-    padding: 0;
-    overflow: hidden;
-    height: max-content;
-    width: min-content;
-    margin-bottom: 10vh;
-    animation-name: zoomOut;
-    animation-duration: 0.75s;
-    animation-timing-function: ease-in-out;
+  background-color: #6e05f2;
+  border-radius: 5%;
+  padding: 0;
+  overflow: hidden;
+  height: max-content;
+  width: min-content;
+  /* margin-bottom: 10vh; */
+  border: 3px solid;
+  border-color: black;
+  cursor: pointer;
+  /* animation-name: zoomOut;
+  animation-duration: 0.75s;
+  animation-timing-function: ease-in-out; */
 }
 
 .thumbnail {
-    width: 39vw;
+  width: 39vw;
 }
 
 .info {
-    text-align: start;
-    color: white;
-    padding-left: 1rem;
-    margin: 1rem 0rem 2rem;
+  text-align: start;
+  color: white;
+  padding-left: 1rem;
+  margin: 1rem 0rem 2rem;
 }
 
 .title {
-    font-size: 1.5rem;
-    
+  font-size: 1.5rem;
 }
 .category {
-    font-size: 0.8rem;
+  font-size: 0.8rem;
 }
 
 @keyframes zoomOut {
-    0% {
-        opacity: 0%;
-        transform: scale(0.1);
-    }
+  0% {
+    opacity: 0%;
+    transform: scale(0.1);
+  }
 
-    100% {
-        opacity: 100%;
-        transform: scale(1.0);
-    }
+  100% {
+    opacity: 100%;
+    transform: scale(1);
+  }
 }
 </style>
