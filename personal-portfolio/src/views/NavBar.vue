ccf3f9191e75ab56a431ebe19e0f3b611dd3d8e0
Sainitin D <s.daverpally@gmail.com>
2024-08-14 18:13:07 -0400

    center navbar

--- personal-portfolio/src/views/NavBar.vue
+++ personal-portfolio/src/views/NavBar.vue
@@ -1,31 +1,52 @@
 <template>
-  <div id="desktop-nav">
-    <div class="logo">Sainitin.</div>
-    <div class="nav-links">
-      <a :style="{ color: '#6e06f2' }" href="#home">Home</a>
-      <a href="#projects">Projects</a>
-      <a href="#work">Work</a>
-      <a href="#skills">Skills</a>
-      <a href="/resume">Resume</a>
-      <a href="#contact">Contact</a>
+  <div id="nav-styling">
+    <div id="desktop-nav">
+      <div class="logo">SainitinD<span class="purple">.</span></div>
+      <div class="nav-links">
+        <a :style="{ color: '#6e06f2' }" href="#home">Home</a>
+        <a href="#projects">Projects</a>
+        <a href="#work">Work</a>
+        <a href="#skills">Skills</a>
+        <a href="/resume">Resume</a>
+        <a href="#contact">Contact</a>
+      </div>
+      <div class="chat">Let's Chat</div>
     </div>
-    <div class="chat">Chat</div>
   </div>
 </template>
 
 <style scoped>
+#nav-styling {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  /* position: sticky;
+  top: 10px; */
+}
+
 #desktop-nav {
   display: flex;
   justify-content: space-between;
   gap: 1rem;
   padding: 0.75rem 0rem;
+  width: 100%;
+  max-width: 1300px;
+  font-size: 1.125rem;
+}
+
+.logo {
+  font-weight: bold;
+}
+
+.logo > span {
+  font-size: 1.5rem;
 }
 
 .nav-links {
   display: flex;
   flex-direction: row;
   gap: 1rem;
-  font-size: 1.125rem;
+
 }
 
 a {
@@ -37,6 +58,10 @@ a {
 
 a:hover {
   color: #6e06f2;
+}
+
+.chat {
+  font-weight: 400;
 }
 
 /* 
